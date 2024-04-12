66980ba64d09e7ac1c978e9511eac8a234cdccfa
Omphile Morwane <omphilemorwane@gmail.com>
2024-04-12 16:20:50 +0200

    updating try catch

--- index.js
+++ index.js
@@ -1,53 +1,57 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=Holiday destinations")
-    .then(res => res.json())
-    .then(data => {
+try {
+    const res = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=Holiday destinations")
+    const data = await res.json()
         document.body.style.backgroundImage = `url(${data.urls.regular})`
         document.getElementById("author").textContent = `By: ${data.user.name}`
-    })
+} catch (err) {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+)`
+    document.getElementById("author").textContent = `By: Dodi Achmad`
+}
 
-fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-    .then(res => {
+
+ 
+try {
+    const res = await fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+
         if (!res.ok) {
             throw Error("Something went wrong")
         }
-        return res.json()
-    })
-    .then(data => {
+        const data = await res.json()
 
         document.getElementById("crypto-top").innerHTML =`
         <img src=${data.image.small}/>
         <span>${data.name}</span>
-        `;
+        `
       document.getElementById("crypto").innerHTML +=`
             <p>🎯: $${data.market_data.current_price.usd}</p>
             <p>⬆️:$${data.market_data.high_24h.usd}</p>
             <p>⬇️:$${data.market_data.low_24h.usd}</p>
       `; 
-    })
-    .catch(err => console.error(err))
+} catch (err) {
+        console.erroe(err)
+    }
 function getCurrentTime() {
 const date = new Date()
 document.getElementById("time").textContent= date.toLocaleTimeString("en-us", {timeStyle: "short"})
 }
 
-setInterval(function() {}, 1000)
+setInterval(getCurrentTime, 1000)
 
-navigator.geolocation.getCurrentPosition(position => {
-    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
-    .then(res=> {
+navigator.geolocation.getCurrentPosition(async position => {
+    try {
+        const res = await fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
             if (!res.ok) {
                 throw Error("Weather data not available")
             }
-            return res.json()
-        })
-        .then(data=> {
+            const data = await res.json()
             const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
             document.getElementById("weather"). innerHTML = `
             <img src=${iconUrl}/>
             <p class="weather-temp">${Math.round(data.main.temp)}º</p>
             <p class="weather-city">${data.name}</p>
-            `;
-        })
-        .catch(err=> console.error(err))
-        
+            `
+        } catch(err) { 
+            console.error(err)
+        }
 });
