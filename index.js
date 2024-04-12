b69d28ec3008d6101f64b7ebfe04fe32161ef8f2
Omphile Morwane <omphilemorwane@gmail.com>
2024-04-12 15:24:03 +0200

    Added the weather Icon,updaded CSS and html

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=money")
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=Holiday destinations")
     .then(res => res.json())
     .then(data => {
         document.body.style.backgroundImage = `url(${data.urls.regular})`
@@ -17,12 +17,12 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
         document.getElementById("crypto-top").innerHTML =`
         <img src=${data.image.small}/>
         <span>${data.name}</span>
-        `
+        `;
       document.getElementById("crypto").innerHTML +=`
             <p>🎯: $${data.market_data.current_price.usd}</p>
             <p>⬆️:$${data.market_data.high_24h.usd}</p>
             <p>⬇️:$${data.market_data.low_24h.usd}</p>
-      `  
+      `; 
     })
     .catch(err => console.error(err))
 function getCurrentTime() {
@@ -31,3 +31,23 @@ document.getElementById("time").textContent= date.toLocaleTimeString("en-us", {t
 }
 
 setInterval(function() {}, 1000)
+
+navigator.geolocation.getCurrentPosition(position => {
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+    .then(res=> {
+            if (!res.ok) {
+                throw Error("Weather data not available")
+            }
+            return res.json()
+        })
+        .then(data=> {
+            const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+            document.getElementById("weather"). innerHTML = `
+            <img src=${iconUrl}/>
+            <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+            <p class="weather-city">${data.name}</p>
+            `;
+        })
+        .catch(err=> console.error(err))
+        
+});
