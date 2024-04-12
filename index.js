d2bfcc2780ef697ab72ccdda5de15f2dccc126a6
Omphile Morwane <omphilemorwane@gmail.com>
2024-04-12 14:14:23 +0200

    Display the time on the screen

--- index.js
+++ index.js
@@ -25,3 +25,9 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
       `  
     })
     .catch(err => console.error(err))
+function getCurrentTime() {
+const date = new Date()
+document.getElementById("time").textContent= date.toLocaleTimeString("en-us", {timeStyle: "short"})
+}
+
+setInterval(function() {}, 1000)
